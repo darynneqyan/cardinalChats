214e8ecdc4ee2f5698f5c52363a0c84bf89ebd5b
merveondogan <merveo@stanford.edu>
2025-05-04 14:09:38 -0700

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth, getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { firebaseConfig } from './firebase.config';
@@ -16,9 +15,7 @@ let storage;
 // Initialize Firebase only if it hasn't been initialized
 if (getApps().length === 0) {
   app = initializeApp(firebaseConfig);
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-  });
+  auth = getAuth(app);
   db = getFirestore(app);
   storage = getStorage(app);
 } else {
